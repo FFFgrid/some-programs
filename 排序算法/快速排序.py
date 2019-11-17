e7b325c795a89a4647ca4386863c1022a2df9c92
Yanyuxin <47472993+FFFgrid@users.noreply.github.com>
2019-11-18 00:30:01 +0800

    Update 快速排序.py

--- 排序算法/快速排序.py
+++ 排序算法/快速排序.py
@@ -1,8 +1,9 @@
 def quicksort(seq):
-    if len(seq) < 2:
+    if len(seq) < 2: #当序列只有一个元素时就不需要排序
         return seq
     else:
-        base = seq[0]
+        base = seq[0] #核心思想就是找一个数作为一个基准数(可以直接取第一个数)，将剩下的数分为比基准小的(left)和比基准大的(right)两组
         left = [elem for elem in seq[1:] if elem < base]
         right = [elem for elem in seq[1:] if elem > base]
-        return quicksort(left) + [base] + quicksort(right)
+        return quicksort(left) + [base] + quicksort(right) #利用递归循环此过程
+    
